93f42911fc32a3a212e03c01d43a8958ea08961c
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-28 15:03:54 +0530

    fixed: update port

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,5 @@ app.use("/api/buyings", buyingRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
