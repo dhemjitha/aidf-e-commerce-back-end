14de13a51684569e6aa09070a8d57329896e2b89
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-28 14:05:48 +0530

    feat: new deployment json

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,5 @@ app.use("/api/buyings", buyingRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-const PORT = 8000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
